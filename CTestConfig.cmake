7bd8249cf785cafc456d8ea0258bf0d81ab6f68b
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2015-06-19 12:39:22 -0400

    Add to CDash

---
+++ CTestConfig.cmake
@@ -0,0 +1,5 @@
+set(CTEST_PROJECT_NAME "meta")
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "192.168.200.81")
+set(CTEST_DROP_LOCATION "/submit.php?project=meta")
+set(CTEST_DROP_SITE_CDASH TRUE)
